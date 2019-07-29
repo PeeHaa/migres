fcaba2e57cb47cfb4a3b8a71720e2d500c6a45d1
Pieter Hordijk <info@pieterhordijk.com>
2019-07-29 23:21:02 +0300

    Added command tests

---
+++ tests/Unit/Command/CreateNewMigrationTest.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace PeeHaa\MigresTest\Unit\Command;
+
+use PeeHaa\Migres\Cli\Output;
+use PeeHaa\Migres\Cli\VerbosityLevel;
+use PeeHaa\Migres\Command\CreateNewMigration;
+use PeeHaa\Migres\Configuration\Configuration;
+use PeeHaa\MigresTest\Fakes\Climate;
+use PHPUnit\Framework\TestCase;
+
+class CreateNewMigrationTest extends TestCase
+{
+    public function testRun(): void
+    {
+        $climate = $this->createMock(Climate::class);
+
+        $climate
+            ->expects($this->once())
+            ->method('br')
+        ;
+
+        $climate
+            ->expects($this->once())
+            ->method('lightGreen')
+            ->willReturnCallback(function (string $message) use ($climate) {
+                preg_match('~^Created new migration at (?P<filename>.*)~', $message, $matches);
+
+                unlink($matches['filename']);
+
+                return $climate;
+            })
+        ;
+
+        $configuration = Configuration::fromArray([
+            'migrationPath' => DATA_DIRECTORY . '/migrations',
+            'namespace'     => 'Foo\Bar',
+            'database'      => [
+                'name'     => 'test_db',
+                'host'     => 'localhost',
+                'port'     => 5432,
+                'username' => 'username',
+                'password' => 'password',
+            ],
+        ]);
+
+        $command = new CreateNewMigration(
+            $configuration,
+            new Output($climate, new VerbosityLevel()),
+            'CreateSomeTable'
+        );
+
+        $command->run();
+    }
+}
