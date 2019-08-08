b979f09b323fa91298b9d6d3dd201b6e0ab74c60
Pieter Hordijk <info@pieterhordijk.com>
2019-08-08 22:28:06 +0300

    Fixed foreign key constraint rollbacks
    The alter table part was missing before

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -25,7 +25,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Fixed
 
-*None*
+- Foreign key constraints can now be rolled back properly
 
 ### Security
 
--- src/Action/AddForeignByQuery.php
+++ src/Action/AddForeignByQuery.php
@@ -7,17 +7,25 @@ use PeeHaa\Migres\Specification\Label;
 
 final class AddForeignByQuery extends TableAction implements Action
 {
+    private Label $name;
+
     private string $query;
 
-    public function __construct(Label $tableName, string $query)
+    public function __construct(Label $tableName, Label $name, string $query)
     {
         parent::__construct($tableName);
 
+        $this->name  = $name;
         $this->query = $query;
     }
 
     public function toQueries(): Queries
     {
-        return new Queries($this->query);
+        return new Queries(sprintf(
+            'ALTER TABLE "%s" ADD CONSTRAINT "%s" %s',
+            $this->tableName->toString(),
+            $this->name->toString(),
+            $this->query,
+        ));
     }
 }
